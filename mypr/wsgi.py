aaf0a4e199230731e815b769c0619abf9942caad
Arpit <arpitjaiswal9526@gmail.com>
2023-03-24 11:47:08 +0530

    Commit

--- mypr/wsgi.py
+++ mypr/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mypr.settings')
 
 application = get_wsgi_application()
+
+####New
+app = application
